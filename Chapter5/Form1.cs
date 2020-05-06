3cb33856d689003dd3e1c1251fab6471c0891d0e
omnis <40761437+ioklkiol@users.noreply.github.com>
2020-05-06 17:51:13 +0800

    更改文件夹名字

---
+++ Chapter5/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chapter4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+      
+        public Vector3D RandomInUnitShpere()
+        {
+            Vector3D p = new Vector3D();
+            do
+            {
+                //RandomDouble生成的是0~1的随机数，然后乘以2再减去1，得到的p的每一个分量均位于-1到1，其实它的范围是一个正方体，
+                //但我们要求的是球内随机点，所以判断一下x*x+y*y+z*z是否大于1
+                p = 2 * new Vector3D(Tools.RandomDouble(), Tools.RandomDouble(), Tools.RandomDouble()) - new Vector3D(1, 1, 1);
+            } while (p.SquaredMagnitude() >= 1);       //如果x*x+y*y+z*z大于1，则说明这个点在正方体内但不在球内，需要重新找
+            return p;
+        }
+
+        private Vector3D GetColor(Ray r, HitableList world, int depth)
+        {
+            HitRecord rec;
+            if (world.Hit(r, 0.001, double.MaxValue, out rec))
+            {
+                Ray scattered;
+                Vector3D attenuation;
+                if (depth < 50 && rec.matPtr.Scatter(r, rec, out attenuation, out scattered))   //只递归50次，避免无谓的性能浪费
+                {
+                    Vector3D color = GetColor(scattered, world, depth + 1);      //每次光线衰减之后深度加一
+                    return new Vector3D(attenuation.X * color.X, attenuation.Y * color.Y, attenuation.Z * color.Z);
+                }
+                else
+                {
+                    return new Vector3D(0, 0, 0);
+                }
+            }
+            else
+            {
+                Vector3D unitDirection = r.Direction.UnitVector();
+                double t = 0.5 * (unitDirection.Y + 1);
+                return (1 - t) * new Vector3D(1, 1, 1) + t * new Vector3D(0.5, 0.7, 1);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int nx = 200;                       //渲染这一章的图需要的时间比较长，可能需要四五个小时，建议在relese模式下进行，也可以改小分辨率和采样率
+            int ny = 100;
+            int ns = 100;
+
+            Bitmap bmp = new Bitmap(nx, ny);
+
+            Vector3D lookAt = new Vector3D(0, 0, 0);
+            Vector3D lookFrom = new Vector3D(13, 2, 3);
+            double diskToFocus = 10;
+            double aperture = 0;
+            Camera cam = new Camera(lookFrom, lookAt, new Vector3D(0, 1, 0), 20,
+                (double)nx / (double)ny, aperture,  diskToFocus,0,1);
+
+
+            HitableList world = TwoPerlinSpheres();
+            for (int j = 0; j < ny; j++)
+            {
+                for (int i = 0; i < nx; i++)
+                {
+                    Vector3D color = new Vector3D(0, 0, 0);
+                    for (int s = 0; s < ns; s++)
+                    {
+                        double u = (double)(i + Tools.RandomDouble()) / (double)nx;
+                        double v = 1 - (double)(j + Tools.RandomDouble()) / (double)ny;
+                        Ray ray = cam.GetRay(u, v);
+                        color += GetColor(ray, world, 0);      //将所有采样点的颜色相加
+                    }
+                    color /= ns;                            //除以采样点的数量得到平均值
+                    color = new Vector3D(Math.Sqrt(color.X), Math.Sqrt(color.Y), Math.Sqrt(color.Z));//进行伽马校正
+                    int r = (int)(255 * color.X);
+                    int g = (int)(255 * color.Y);
+                    int b = (int)(255 * color.Z);
+                    bmp.SetPixel(i, j, Color.FromArgb(r, g, b));
+
+                }
+
+            }
+            pictureBox1.BackgroundImage = bmp;
+        }
+
+        private HitableList TwoPerlinSpheres()
+        {
+            Texture pertext = new NoiseTexture();
+            List<IHitable> list = new List<IHitable>();
+           
+                
+            list.Add(new Sphere(new Vector3D(0, -1000, 0), 1000, new Lambertian(pertext)));
+            list.Add(new Sphere(new Vector3D(0, 2, 0), 2, new Lambertian(pertext)));
+            
+            return new HitableList(list, list.Count);
+
+        }
+
+    }
+}
---
+++ Chapter5/Perlin.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Perlin
+{
+    private static double[] ranDouble = PerlinGenerate();
+    private static int[] permX = PerlinGeneratePerm();
+    private static int[] permY = PerlinGeneratePerm();
+    private static int[] permZ = PerlinGeneratePerm();
+
+    public double Noise(Vector3D p)
+    {
+        double u = p.X - Math.Floor(p.X);
+        double v = p.Y - Math.Floor(p.Y);
+        double w = p.Z - Math.Floor(p.Z);
+        int i = (int)Math.Floor(p.X);
+        int j = (int)Math.Floor(p.Y);
+        int k = (int)Math.Floor(p.Z);
+        double[][][] c = new double[2][][];
+        for (int a = 0; a < 2; a++)
+        {
+            c[a] = new double[2][];
+            for (int b = 0; b < 2; b++)
+            {
+                c[a][b] = new double[2];
+            }
+        }
+        for (int di = 0; di < 2; di++)
+            for (int dj = 0; dj < 2; dj++)
+                for (int dk = 0; dk < 2; dk++)
+                    c[di][dj][dk] = ranDouble[permX[(i + di) & 255] ^
+                      permY[(j + dj) & 255] ^ permZ[(k + dk) & 255]];
+        return TrilinearInterp(c, u, v, w);
+    }
+    public static void Permute(int[] p, int n)
+    {
+        for (int i = n - 1; i > 0; i--)
+        {
+            int target = (int)(Tools.RandomDouble() * (i + 1));
+            int temp = p[i];
+            p[i] = p[target];
+            p[target] = temp;
+        }
+        return;
+    }
+
+    public static double[] PerlinGenerate()
+    {
+        double[] p = new double[256];
+        for (int i = 0; i < 256; i++)
+            p[i] = Tools.RandomDouble();
+        return p;
+    }
+    public static int[] PerlinGeneratePerm()
+    {
+        int[] p = new int[256];
+        for (int i = 0; i < 256; i++)
+            p[i] = i;
+        Permute(p, 256);
+        return p;
+    }
+    public static double TrilinearInterp(double[][][] c, double u, double v, double w)
+    {
+        double accum = 0;
+        for (int i = 0; i < 2; i++)
+            for (int j = 0; j < 2; j++)
+                for (int k = 0; k < 2; k++)
+                    accum += (i * u + (1 - i) * (1 - u)) *
+                        (j * v + (1 - j) * (1 - v)) *
+                        (k * w + (1 - k) * (1 - w)) * c[i][j][k];
+        return accum;
+    }
+}
