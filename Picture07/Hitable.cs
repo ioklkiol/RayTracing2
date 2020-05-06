9ddfe04e81c00dac8f2d79d94b584e59d3139c93
omnis <40761437+ioklkiol@users.noreply.github.com>
2020-05-06 22:19:59 +0800

    perlin噪声

---
+++ Picture07/Hitable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public interface  IHitable
+{
+    bool Hit(Ray r, double tMin, double tMax,out HitRecord rec);
+    bool BoundingBox(double t0, double t1,out AABB box);
+} 
+
